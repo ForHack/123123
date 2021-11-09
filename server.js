10dedbf0f3de26256d0540ee8bf83d965fbc1913
ForHack <olimjonov-odiljon@mail.ru>
2021-11-09 23:29:46 +0500

    3th part is end

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB()
 app.use(express.json({ extended: false }))
 
 //Define Routes
-app.use('/api/user', require('./routes/api/users'))
+app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
